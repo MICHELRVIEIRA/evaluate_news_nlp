28044eed54d79fb5b7824bbb88f01b1cf6e37714
MICHEL <michelrvieira@gmail.com>
2021-03-11 16:33:46 -0300

    Ajustado até a aula 2 do curso - Ok

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const mockAPIResponse = require('./mockAPI')
 
 const app = express()
 
@@ -11,13 +11,13 @@ var textapi = new aylien({
     application_key: process.env.API_KEY
 });
 
-app.use(express.static('dist'))
+app.use(express.static('/dist'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('/dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
