0c47aeb1446efc6861a1820f574bc4d82f5823f3
MICHEL <michelrvieira@gmail.com>
2021-03-15 21:19:54 -0300

    Inclusão do jest e regex

---
+++ __tests__/updateUI.test.js
@@ -0,0 +1,6 @@
+import { updateUI } from "../src/client/js/updateUI"
+
+describe("Testing the page update based on api data", () => {
+    test("Testing the updateUI() function", () => {
+        expect(updateUI).toBeDefined();
+})});
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,52 +4,24 @@ function handleSubmit(event) {
 
     let texto = document.getElementById('name').value
 
-    fetch('http://localhost:8081/meaningcloud-api', {
+    if (!/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(texto)){
+      fetch('http://localhost:8081/meaningcloud-api', {
       method: 'POST', 
       credentials: 'same-origin', 
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({texto: texto})  
-    })
+      })
 
       .then(res => res.json())  
       .then(res=>{Client.updateUI(res)})
-
-    //console.log("::: Form Submitted :::")
-    //fetch('http://localhost:8080/test')
-    //.then(res => res.json())
-    //.then(function(res) {
-    //    document.getElementById('results').innerHTML = res.message
-    //})
+    }
+    
+    else {
+      alert("It was not possible to analyze the inserted text.")
+    }
+   
 }
-
-// Código ajustado conforme instruções contidas na questão 517022, do fórum do curso.
-
-// Link: https://knowledge.udacity.com/questions/517022
-
-/* const updateUI  = async () => {
-  
-    const request = await fetch ('/')
-  
-    try {
-  
-      const allData = await request.json();
-      
-      console.log(allData);
-  
-      document.getElementById('score').innerHTML = allData.score;
-      document.getElementById('confidence').innerHTML = allData.confidence;
-      document.getElementById('irony').innerHTML = allData.irony;
-  
-    }
-  
-    catch (error){
-  
-      console.log("error", error);
-  
-    }
-  
-}*/
 
 export { handleSubmit }
